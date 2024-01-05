a60df6cf7022359810c1c0d6133ac3f62f6821a5
juris12 <102772784+juris12@users.noreply.github.com>
2024-01-05 15:22:22 +0200

    worng aproach

--- test.js
+++ test.js
@@ -15,35 +15,32 @@ async function getRandomText() {
     try {
       const response = await fetch('https://baconipsum.com/api/?type=meat-and-filler');
       const data = await response.json();
-      return data[0];
+      return data.join(' ');
     } catch (error) {
       console.error('Error fetching random text:', error);
       return ''
     }
   }
-const add_text = async (testTyping) => {
-    let text = ''
-    getRandomText().then(random => {
-        random.split(' ').forEach(word => {
-            const spanElement = document.createElement('span');
-            spanElement.className = 'test_span';
-            spanElement.textContent = word;
-            spanElement.id = '';
-            
-            testTyping.appendChild(spanElement);
-        });
-        game.text = [...game.text,...text.split(' ')]
-        
-    })
+  const add_text = async (testTyping) => {
+    const data = await getRandomText()
+    data.split(' ').filter((val) => val !== '').forEach(word => {
+        const spanElement = document.createElement('span');
+        spanElement.className = 'test_span';
+        spanElement.textContent = word;
+        spanElement.id = '';
+        testTyping.appendChild(spanElement);
+    });
+    game.text = [...game.text,...data.split(' ').filter((val) => val !== '')]
+    console.log('-----------------------------------------')
+    return document.querySelectorAll('.test_span');
 }
 
 
 document.addEventListener("DOMContentLoaded", async function() {
-
+    testSpans = await add_text(testTyping);
     let currentIndex = 0;
     const set_curent_span = async (i) => {
-        await add_text(testTyping);
-        testSpans.forEach(leater => {
+         testSpans.forEach(leater => {
             leater.id = ''
         })
         
@@ -53,13 +50,15 @@ document.addEventListener("DOMContentLoaded", async function() {
 
 
 
-    function handleKeyPress(event) {
+    async function handleKeyPress() {
         game.startTimer(); // Start the timer
         curentWord.innerHTML = ''
         let curentWordIndex = 0
-        let row = 0
+        let removeIndex = 0
+        let row = 0 
+        
         let rowWidth = 0
-        document.addEventListener("keyup", function(event) {
+        document.addEventListener("keyup", async function(event) {
             if (/^[a-zA-Z0-9]$/.test(event.key)) {
                 const isShiftPressed = event.shiftKey;
                 const pressedKey = isShiftPressed ? event.key.toUpperCase() : event.key;
@@ -71,32 +70,47 @@ document.addEventListener("DOMContentLoaded", async function() {
                 }
                 curentWordIndex++
             } else if (event.key === ' ') {
-                rowWidth += testSpans[row].offsetWidth
-                console.log(row + ' ' + testSpans[row].offsetWidth+ ' ' +  rowWidth + ' ' + testSpans.length)
-                if (rowWidth >= testTyping.offsetWidth-50) {
-                    currentIndex -= row
-                    rowWidth = 0 
-                    for (let i = 0; i < row; i++) {
-                        if (testTyping.firstChild) {
+                rowWidth += testSpans[removeIndex].offsetWidth
+                console.log(game.text[currentIndex],testSpans[currentIndex].innerHTML,testSpans.length,game.text.length,removeIndex,row, currentIndex)
+                if(game.currentWord === game.text[currentIndex]) {
+                    testSpans[currentIndex].classList.add('test_span_old')
+                } else {
+                    testSpans[currentIndex].classList.add('test_span_error')
+                }
+                if (rowWidth >= testTyping.offsetWidth) {
+                    if(row == 2){
+                        row = 0
+                        console.log('+++++++++++++++++++++++++++++++++++++++')
+                        testSpans = await add_text(testTyping);
+                        testSpans = document.querySelectorAll('.test_span')
+                        
+                        console.log('ll',row)
+                    } else {
+                        currentIndex -= removeIndex
+                        row++
+                        rowWidth = 0 
+                        game.text.splice(0,removeIndex) 
+                        while (testTyping.firstChild && removeIndex > 0) {
                             testTyping.removeChild(testTyping.firstChild);
+                            removeIndex--;
                         }
                     }
-                    row = 0
+
                     testSpans = document.querySelectorAll('.test_span')
                 }
-                if (game.currentWord == game.text[currentIndex]) {
-                    game.words++
-                    testSpans[currentIndex].className += ' test_span_old'    
-                } else {
-                    testSpans[currentIndex].className += ' test_span_error' 
-                }
+
+
+                removeIndex++
                 game.allWords++
                 game.currentWord = ''
                 currentIndex++
-                row++
                 set_curent_span(currentIndex)
                 curentWord.innerText = ''
                 curentWordIndex = 0
+
+
+
+
             } else if (event.key === 'Backspace') {
                   game.currentWord = game.currentWord.slice(0,-1)
                 curentWord.removeChild(curentWord.lastChild)
